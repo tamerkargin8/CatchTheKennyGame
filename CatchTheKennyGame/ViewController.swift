437cc36c35ad5885010d9345594044bea6fb3d43
Tamer KARGIN <96518304+tamerkargin8@users.noreply.github.com>
2023-04-11 22:21:23 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Tamer KARGIN on 11.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
